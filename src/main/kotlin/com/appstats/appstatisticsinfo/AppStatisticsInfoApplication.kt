2b4d66102582896ee76e8c7238fe5e2fc32a184c
siaVash <siavashtf@gmail.com>
2020-08-31 00:20:05 +0430

    Initilizing project using Spring Initialzr.

---
+++ src/main/kotlin/com/appstats/appstatisticsinfo/AppStatisticsInfoApplication.kt
@@ -0,0 +1,11 @@
+package com.appstats.appstatisticsinfo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AppStatisticsInfoApplication
+
+fun main(args: Array<String>) {
+	runApplication<AppStatisticsInfoApplication>(*args)
+}
---
+++ src/test/kotlin/com/appstats/appstatisticsinfo/AppStatisticsInfoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.appstats.appstatisticsinfo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AppStatisticsInfoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
